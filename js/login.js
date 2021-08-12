5109f614b81e629a73fc9d90a52257b186a12a86
SantiM96 <smt093@gmail.com>
2021-08-12 09:47:07 -0300

    add product and initial add of google auth

--- js/login.js
+++ js/login.js
@@ -1,6 +1,73 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
+
+    
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+        apiKey: "AIzaSyAPc31YV2I2sWfbD42B0E9wPigqaR_ys6I",
+        authDomain: "chatfirebase-practice.firebaseapp.com",
+        projectId: "chatfirebase-practice",
+        storageBucket: "chatfirebase-practice.appspot.com",
+        messagingSenderId: "761978211647",
+        appId: "1:761978211647:web:f7196a0ad90bbb0a11178a"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+
+    firebase.auth().onAuthStateChanged((user) => {
+        //logged user
+        if (user) {
+            userAuth(user)
+            chatContent(user)
+            hideButton("logIn")
+        }
+        //user not logged
+        else {
+            userWithoutAuth()
+            hideButton("logOut")
+        }
+    })
+    
+    const userWithoutAuth = () => {
+        console.log('usuario sin registrar')
+        form.classList.add('d-none')
+        webContent.innerHTML = `
+            <p id="startSesion" class="lead text-center my-5">Debes iniciar sesión</p>
+        `
+        const provider = new firebase.auth.GoogleAuthProvider()
+        btnLogIn.onclick = () => {
+            firebase.auth().signInWithPopup(provider).then((result) => {
+                const user = result.user
+                userName.innerHTML = user.displayName
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code
+                // The email of the user's account used.
+                const email = error.email
+            })
+        }
+
+    }
+
+    const userAuth = user => {
+        console.log('usuario registrado')
+        const startSesion = document.getElementById('startSesion')
+        form.classList.remove('d-none')
+        if (startSesion) webContent.removeChild(startSesion)
+        userName.innerHTML = user.displayName
+
+        btnLogOut.onclick = () => {
+            firebase.auth().signOut().then(() => {
+                // Sign-out successful.
+                userName.innerHTML = "BChat"
+            }).catch((error) => {
+                // An error happened.
+                console.log(error)
+            })
+        }
+    }
 
 });
--- js/products.js
+++ js/products.js
@@ -3,4 +3,35 @@
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
 
+    async function showProductsList(){
+
+        const products = await getJSONData(PRODUCTS_URL)
+        console.log(products.data)
+
+        for (let i = 0; i < products.data.length; i++){
+        // for(product of products.data){
+
+            document.getElementById("products-list-container").innerHTML += `
+                <a href="products-info.html" class="list-group-item list-group-item-action">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="` + products.data[i].imgSrc + `" alt="` + products.data[i].description + `" class="img-thumbnail">
+                        </div>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h4 class="mb-1">`+ products.data[i].name +`</h4>
+                                <small class="text-muted">` + products.data[i].soldCount + ` artículos</small>
+                            </div>
+                            <p class="mb-1">` + products.data[i].description + `</p>
+                            <p class="mb-1">${products.data[i].currency} ${products.data[i].cost}</p>
+                        </div>
+                    </div>
+                </a>
+            `
+
+        }
+    }
+
+    showProductsList()
+
 });
